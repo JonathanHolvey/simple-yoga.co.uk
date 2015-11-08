80f59779332828058edb3a208295457a1e2a6353
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-11-08 13:35:01 +1000

    Fix headings on classes page, rename map link and remove comment blocks

--- classes.php
+++ classes.php
@@ -16,40 +16,23 @@
 	<?php include("header.php"); ?>
 	
 	<div class="content">
-		<!--div class="title">Simple Yoga at Freshford Village Hall</div>
-		<p>Near Bath - <a href="http://maps.google.co.uk/maps?hl=en&ll=51.339004,-2.310884&spn=0.011863,0.033023&hnear=Bristol,+United+Kingdom&t=m&z=16&iwloc=lyrftr:m,16501280131153264604,51.339004,-2.310905">Click here for map</a></p>
-		<p>Thursdays 2:00-3:00 PM</p>
-		<p>&pound; 5.00 for your first class and &pound; 25.00 for a block of five. A single drop in session is &pound; 6.00.</p>
-		<p>Wear comfortable clothing and bring a mat and blanket for relaxation. If you don't have your own mat I have a few you can borrow or purchase.</p>
-		<p>There is plenty of parking at the Village Hall. Please do get in touch if you have any questions.</p-->
-		<!--div class="title">Chair Yoga</div>
-		<p>Chair Yoga around my kitchen table or yours.</p>
-		<p>A gentle yoga class that includes postures to improve the muscle tone of the face as well as the body, improve your posture and help you to look as young as you feel.</p>
-		<p>Private class or invite a few friends to join you. Laughter included!</p-->
-		<!--div class="title">June Face Yoga Workshop</div>
-		<p>Around my kitchen table on 25th June, 10:00 AM until noon.</p><p>Spaces are limited so please call to book in advance.</p><p>&pound;17.50 for two hours.</p-->
-		<div class="title">Yoga class at Limpley Stoke Village Hall</div>
-		<p>Come along for a weekly class in Limpley Stoke, from 9:30 to 11:00&nbsp;AM at the Village Hall, just off the A36 (<a href="https://www.google.com/maps/place/Limpley+Stoke+Village+Hall/@51.3445205,-2.3207854,15.5z/data=!4m6!1m3!3m2!1s0x0000000000000000:0xf2aa6ec9c03a336b!2sLimpley+Stoke+Village+Hall!3m1!1s0x0000000000000000:0xf2aa6ec9c03a336b">click here for map</a>).</p>
+		<h2>Yoga class at Limpley Stoke Village Hall</h2>
+		<p>Come along for a weekly class in Limpley Stoke, from 9:30 to 11:00&nbsp;AM at the Village Hall, just off the A36 - <a href="https://www.google.com/maps/place/Limpley+Stoke+Village+Hall/@51.3445205,-2.3207854,15.5z/data=!4m6!1m3!3m2!1s0x0000000000000000:0xf2aa6ec9c03a336b!2sLimpley+Stoke+Village+Hall!3m1!1s0x0000000000000000:0xf2aa6ec9c03a336b">View map</a></p>
 		<p>We focus on improving suppleness and balance, reducing stress and increasing energy levels. The classes are easy to understand, allowing you to work at your own pace and ability.</p>
 		<p class="offer">Your first class costs <b>&pound;5</b>, and during August 2015 you can bring a friend for free!</p>
 		<p>Drop in classes are priced at <b>&pound;9</b>, while a course of four consecutive classes can be booked for only <b>&pound;30</b>. Please remember to bring along a mat or rug and a blanket. Call or email for more details.
 		<p>Please note that parking at the hall is limited, however additional parking can be found just up the hill at the Rose and Crown.</p>
-		<div class="title">One to One Yoga Class</div>
+		<h2>One to One Yoga Class</h2>
 		<p>In my home or garden (weather permitting) or your home.</p>
 		<p>Suitable for beginners or anyone wanting a simple, gentle class. Adapted to your own abilities and needs. Gentle stretching, moving every part of your body from the tips of your toes to your facial muscles. Working with the breath, we always end with a peaceful guided relaxation which allows you to fully relax and let go, leaving you feeling very peaceful.</p>
 		<p><b>&pound;20</b> for each hour long session, <b>&pound;27</b> for an hour and a half, or book a course of six one hour sessions for only <b>&pound;100</b>.</p>
- 		<div class="title">Yoga for Fatigue</div>
+ 		<h2>Yoga for Fatigue</h2>
 		<p>An extremely gentle class  that could help with <b><i>Chronic Fatigue Syndrome</i></b>, <b><i>M.E.</i></b>, <b><i>Fibromyalgia</i></b>, <b><i>M.S.</i></b>, <b><i>Arthritis</i></b>, <b><i>Stress</i></b> or any other illness that has left you feeling fatigued.</p>
 		<p>I have personal experience of chronic fatigue in my family and I am fully aware of how this can impact on your life. Having found how helpful yoga is for this condition, I trained with Fiona Agombar in London to teach Yoga Therapy for M.E., Chronic Fatigue Syndrome and Chronic Illness.</p>
 		<p>I keep postures very gentle and emphasis concentration on the breath which allows you to fully relax and in turn leads to increased energy.</p>
 		<p>I teach very intuitively, we work at your own pace, only doing what you feel you can manage on the day. Most postures are done lying down or sitting in a chair, wherever you feel most comfortable. We finish with a long guided relaxation which allows you to fully relax. You can benefit from this gentle, relaxing class, no matter what your energy levels are like.</p>
 		<p>I teach private classes, or one to one sessions in your home. Please phone for a supportive chat or to arrange a free visit.</p>
-<!-- 		<div class="title">Face Yoga</div>
-		<p>Yoga helps tone and relax the body and your face is no different. With face yoga we start with balancing our body, then really study our face, noting tension and long held habits, and work on retraining our facial muscles. This helps to lift, smooth and improve our skin tone. We use massage and relaxation techniques, and laughter is included!</p>
-		<p>Even more importantly, we accept and enjoy what we cannot change, allowing us to relax, and remove tension and stress which can show clearly in our faces.</p> -->
-<!-- 		<div class="title">Simple yoga in local schools and Care Homes</div>
-		<p>Teaching the young and not so young is a joy. Please phone for further details.</p>
- -->		<?php printQuote() ?>
+		<?php printQuote() ?>
 	</div>
 	
 	<?php include("footer.php"); ?>
