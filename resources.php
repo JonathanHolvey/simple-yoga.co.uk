07b28ee119ab471fe15e2dad90c0301240b2ed89
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-11-08 12:43:21 +1000

    Replace JavaScript generated quote marks with CSS pseudo elements

--- resources.php
+++ resources.php
@@ -2,6 +2,6 @@
 <link rel="stylesheet" type="text/css" href="fonts/fonts.css"/>
 <link rel="stylesheet" type="text/css" href="styles.css"/>
 <script type="text/javascript" src="script/jquery.js"></script>
-<script type="text/javascript" src="script/common.js"></script>
+<!-- <script type="text/javascript" src="script/common.js"></script> -->
 <link rel="shortcut icon" href="images/favicon.ico" />
 <?php include("script/printQuote.php") ?>
--- script/common.js
+++ script/common.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-	// add quote marks to quotes
-	$(".quote").prepend("<span class=\"open\">&ldquo;</span>");
-	$(".quote").append("<span class=\"close\">&rdquo;</span>");
+
 });
