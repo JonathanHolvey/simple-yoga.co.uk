c0caaa550ed4e550ad412056b18685e9cf8845b6
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-07-27 20:11:39 +1000

    Hard-code first blog post content

--- web/public/blog.php
+++ web/public/blog.php
@@ -15,10 +15,35 @@
 	<?php include("header.php"); ?>
 	
 	<div class="content">
-		<div class="page-text">
-			<h2>Blog title</h2>
-			<p>Blog text</p>
-		</div>
+		<article class="page-text" itemscope="" itemtype="http://schema.org/BlogPosting">
+			<div itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+				<meta itemprop="name" content="Laura Holvey">
+			</div>
+
+			<div itemprop="publisher" itemscope="" itemtype="http://schema.org/Organization">
+				<meta itemprop="name" content="Simple Yoga">
+				<meta itemprop="url" content="https://simple-yoga.co.uk">
+				<meta itemprop="sameAs" content="https://www.facebook.com/Simple-Yoga-459676930772950/">
+			</div>
+
+			<meta itemprop="mainEntityOfPage" content="https://simple-yoga.co.uk/blog/daisies">
+			<meta itemprop="dateModified" content="2019-07-25 17:22:00">
+			<meta itemprop="image" content="https://heirloom-earth.com/bl-content/uploads/190224image.jpg">
+			<meta itemprop="thumbnailUrl" content="https://heirloom-earth.com/bl-content/uploads/thumbnails/190224image.jpg">
+
+			<header>
+				<h1 itemprop="headline">Dasies</h1>
+				<time itemprop="datePublished" datetime="2019-07-25 17:22:00">25 June 2019</time> - by Laura Holvey
+			</header>
+
+			<div itemprop="content">
+				<p>I have been asked why I use a daisy as my logo for Simple Yoga...</p>
+				 
+				<p>I have always loved daisies. I love  their colour, their simplicity, their freshness. I have a very fond memory of  lying in a field, with arms outstretched, daisies all around me, with my face  upturned to the warmth of the sun. Bliss... Then there is the memory of walking  through fields of moon daisies, with a dog called Biggles, on an organic farm  near to where we used to live. We knew the owners and were allowed to roam  wherever we wanted and only had to call out for Biggles when we arrived, and he  would appear, panting, a huge Airedale, always ready for a walk. With such  lovely memories it is hardly surprising that I chose a daisy to be my logo when  I set up Simple Yoga. It seemed to me to represent the simplicity I was craving  in my yoga and my life. As I write this I am looking out on a field and the rain  is chucking it down but all I have to do is close my eyes and think back to  lying in my field of daisies and I feel more peaceful and relaxed.</p>
+				 
+				<p>Another love, which I am sure many of  you reading this will share, is the sea. Lying down on your favourite beach  somewhere, all on your own, not another person in sight. Feeling the warmth of  the sand below and the sun above, breathing in that glorious salty air. Hearing  birdsong as your body releases and relaxes as you breathe in and out, in and  out... Becoming aware of the sound of the waves flowing towards the shoreline...  I share this relaxation, or a version of it as every relaxation is slightly  different at the end of each class and I weave in my version of yoga nidra which  leads you further into your peaceful place. I love teaching this relaxation; it  allows the body to fully relax, calming the mind too. My own body and mind relax  as I sit and slowly and intuitively guide the relaxation; if I was lying down I  might nod off!</p>
+			</div>
+		</article>
 	</div>
 	
 	<?php include("footer.php"); ?>
