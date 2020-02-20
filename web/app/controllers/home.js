c5823961751e1fdcdac30dfbce1a394dfec5ee94
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-02-21 07:52:54 +1000

    Add simple express app, copied from katyholvey.co.uk

---
+++ web/app/controllers/home.js
@@ -0,0 +1,5 @@
+const controller = (req, res) => {
+  res.render('home', { type: 'homepage' })
+}
+
+module.exports = controller
---
+++ web/webpack.config.js
@@ -0,0 +1,44 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const path = require('path')
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'production'
+  return {
+    mode,
+    watch: mode === 'development',
+
+    entry: {
+      main: ['./assets/js/main.js', './assets/scss/main.scss'],
+    },
+
+    output: {
+      path: path.join(__dirname, 'app/public/'),
+      filename: 'js/[name].js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+      new LiveReloadPlugin(),
+    ],
+  }
+}
