f11636016af3df49d20c1492c5fc043cc87a7d8f
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-02-21 18:00:00 +1000

    Add Ghost CMS with express proxy

--- web/app/server.js
+++ web/app/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+const { createProxyMiddleware: proxy } = require('http-proxy-middleware')
 const router = require('./router')
 const logger = require('./middleware/logger')
 
 const app = express()
 const port = 80
+
+// Add proxy access to CMS routes
+app.use('/ghost', proxy({ target: 'http://cms:2368' }))
+app.use('/content', proxy({ target: 'http://cms:2368' }))
 
 app.use(logger)
 app.use(express.json())
