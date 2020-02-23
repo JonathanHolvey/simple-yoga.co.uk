fe0a995de635b24c185722f9eb1909ee98f43ca4
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-02-23 16:18:06 +1000

    Add menu and homepage content from CMS

---
+++ web/app/api/cms.js
@@ -0,0 +1,145 @@
+const GhostContentAPI = require('@tryghost/content-api')
+
+const api = new GhostContentAPI({
+  url: process.env.GHOST_API_URL,
+  key: process.env.GHOST_API_KEY,
+  version: 'v3',
+})
+
+const options = {
+  include: 'tags,authors',
+}
+
+/**
+ * Get the path component of a URL
+ * @param {string} url
+ * @return {string}
+ */
+const getUrlPath = (url) => (
+  url.replace(/^(?:https?:\/\/[^/]+)?(\/.*?)\/?$/, '$1')
+)
+
+/**
+ * Recursively format CMS objects
+ * @param {object} item
+ * @return {object}
+ */
+const format = (item, name) => {
+  if (Array.isArray(item)) {
+    // Handle all array elements
+    return item.map((value) => format(value, name))
+  }
+  if (item && typeof item === 'object') {
+    // Handle all object properties
+    const output = {}
+    Object.entries(item).forEach(([key, value]) => {
+      output[key] = format(value, key)
+
+      if (key === 'url') {
+        // Add relative path property
+        output.path = getUrlPath(value)
+      }
+    })
+    return output
+  }
+  if (name === 'url') {
+    // Remove trailing slashes from URLs
+    return item.replace(/\/$/, '')
+  }
+  return item
+}
+
+/**
+ * Get a single page from the CMS
+ * @param {string} slug
+ * @return {promise}
+ */
+const getPage = (slug) => (
+  api.pages.read({ slug })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get a single post from the CMS
+ * @param {string} slug
+ * @return {promise}
+ */
+const getPost = (slug) => (
+  api.posts.read({ slug }, options)
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get all posts from the CMS
+ * @return {promise}
+ */
+const getPosts = () => (
+  api.posts.browse({ limit: 'all', ...options })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get all posts from the CMS for a particular tag
+ * @param {string} slug - The tag slug
+ * @return {promise}
+ */
+const getTagPosts = (slug) => (
+  api.posts.browse({ limit: 'all', filter: `tag:${slug}`, ...options })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get featured posts from the CMS
+ * @param {number} limit - The number of posts to fetch
+ * @return {promise}
+ */
+const getFeaturedPosts = (limit) => (
+  api.posts.browse({ limit, filter: 'featured:true', ...options })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get a single tag from the CMS
+ * @param {string} slug
+ * @return {promise}
+ */
+const getTag = (slug) => (
+  api.tags.read({ slug })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get all tags from the CMS
+ * @return { promise}
+ */
+const getTags = () => (
+  api.tags.browse({ limit: 'all', include: 'count.posts' })
+    .then(format)
+    .catch(console.error)
+)
+
+/**
+ * Get all settings from the CMS
+ * @return {promise}
+ */
+const getSettings = () => (
+  api.settings.browse()
+    .then(format)
+)
+
+module.exports = {
+  getPage,
+  getPost,
+  getPosts,
+  getTagPosts,
+  getFeaturedPosts,
+  getTag,
+  getTags,
+  getSettings,
+}
--- web/app/controllers/home.js
+++ web/app/controllers/home.js
@@ -1,5 +1,17 @@
-const controller = (req, res) => {
-  res.render('home', { type: 'homepage' })
+const cms = require('../api/cms')
+
+const controller = async (req, res) => {
+  const [page, settings] = await Promise.all([
+    cms.getPage('home'),
+    cms.getSettings(),
+  ])
+
+  console.log(page)
+  res.render('home', {
+    type: 'homepage',
+    menus: settings.navigation,
+    page,
+ })
 }
 
 module.exports = controller
